be3bc819b7c90cd8d5471da6e0d106f15208105b
Timothy Itodo <itodotimothy6@gmail.com>
2019-09-05 21:28:29 -0500

    Add questions

--- 1.1/solution.cpp
+++ 1.1/solution.cpp
@@ -1,3 +1,7 @@
+// Question 1.1
+// Implement an algorithm to determine if a string has all unique characters. What if you
+// can not use additional data structures?
+
 #include <iostream> 
 #include <unordered_set>
 #include <vector>
--- 1.3/solution.cpp
+++ 1.3/solution.cpp
@@ -1,6 +1,12 @@
+// Question 1.3
+// Design an algorithm and write code to remove the duplicate characters in a string
+// without using any additional buffer. NOTE: One or two additional variables are fine.
+// An extra copy of the array is not.
+// FOLLOW UP
+// Write the test cases for this method.
+
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 // O(n^2)
@@ -46,7 +52,6 @@ int main(){
     int n = sizeof(s);
     removeDuplicate(NULL, n-1);
     cout << s << endl;
-
 
     string str = "";
     removeDuplicate_WithDS(str);
