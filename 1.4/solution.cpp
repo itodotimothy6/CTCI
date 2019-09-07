13971dbc24c10903aded8e9c75aca92e9ee25e98
Timileyin <timade55@gmail.com>
2019-09-07 16:46:51 -0500

    My solution to the anagrams problem

--- 1.4/solution.cpp
+++ 1.4/solution.cpp
@@ -2,29 +2,41 @@
 // Write a method to decide if two strings are anagrams or not.
 
 #include <iostream>
-#include <unordered_map>
 
-using namespace std;
-
-bool isAnagram(string s1, string s2) {
-    if (s1.length() != s2.length()) return false;
-
-    unordered_map<char, int> charMap;
-    for (int i = 0; i < s1.length(); i++) {
-        charMap[s1[i]]++;
+bool areAnagrams(string word1, string word2)
+{
+    // Get the lengths of the two strings
+    int len1 = word1.length();
+    int len2 = word2.length();
+    
+    //This is checking if both strings are the same length and if not they cannot be anagrams
+    if (len1 != len2){
+        return false;
     }
-    for (int i = 0; i < s2.length(); i++) {
-        charMap[s2[i]]--;
-        if (charMap[s2[i]] == 0) charMap.erase(s2[i]);
+    
+    // using the sort method to sort both the strings after determining the length
+    sort(word1.begin(), word1.end());
+    sort(word2.begin(), word2.end());
+    
+    // This compares sorted strings to determine if they have the same letters
+    for (int i = 0; i < len1; i++) {
+        if (word1[i] != word2[i]) {
+            return false;
+        }
     }
-
-    return charMap.empty();
+    return true;
 }
 
-int main() {
-    string s1 = "maddam";
-    string s2 = "mfdadm";
-    cout << isAnagram(s1, s2) << endl;
-
+int main()
+{
+    string word1 = "time";
+    string word2 = "mint";
+    if (areAnagrams(word1, word2)){
+        cout<<"Yes this strings are anagrams of each other \n";
+    }
+    else{
+        cout<<"No they are not anagrams of each other \n";
+    }
+    
     return 0;
 }
