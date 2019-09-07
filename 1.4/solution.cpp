bf0465dc1719ac852bbf6e309b1eea0c3224fecf
Faruq Hammed <faruqhammed8@gmail.com>
2019-09-06 19:09:51 -0500

    Update solution.cpp

--- 1.4/solution.cpp
+++ 1.4/solution.cpp
@@ -1,30 +1,37 @@
 // Question 1.4
 // Write a method to decide if two strings are anagrams or not.
 
-#include <iostream>
-#include <unordered_map>
 
+#include <iostream>
+#include <string>
 using namespace std;
 
-bool isAnagram(string s1, string s2) {
-    if (s1.length() != s2.length()) return false;
-
-    unordered_map<char, int> charSet;
-    for (int i = 0; i < s1.length(); i++) {
-        charSet[s1[i]]++;
-    }
-    for (int i = 0; i < s2.length(); i++) {
-        charSet[s2[i]]--;
-        if (charSet[s2[i]] == 0) charSet.erase(s2[i]);
-    }
-
-    return charSet.empty();
+bool areAnagrams(string one, string two){
+	if(one.length() != two.length())
+		return false;
+	else{
+		int anagram[26];
+		for(int i = 0; i < 26; i++)
+			anagram[i] = 0;
+		bool isAnagram = true;
+		
+		for(int i = 0; i < one.length(); i++)
+			anagram[one[i] - 'a']++;
+		for(int i = 0; i < two.length(); i++)
+			anagram[two[i] - 'a']--;
+		for(int i = 0; i < two.length(); i++)
+		{
+			if(anagram[one[i] - 'a'] > 0 )
+				isAnagram = false;
+		}
+		return isAnagram;
+	}
 }
 
-int main() {
-    string s1 = "maddam";
-    string s2 = "mfdadm";
-    cout << isAnagram(s1, s2) << endl;
-
-    return 0;
+int main(){
+	string one = "saecm", two = "semca";
+	cout << "IsAnagrams: " << areAnagrams(one, two);
+	
 }
+
+
