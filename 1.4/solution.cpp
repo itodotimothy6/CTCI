e9f6b8cd9b565cc8628e3e5dab2792f6cfb29f6e
Timothy Itodo <itodotimothy6@gmail.com>
2019-09-06 20:12:19 -0500

    Update solution.cpp

--- 1.4/solution.cpp
+++ 1.4/solution.cpp
@@ -1,37 +1,36 @@
 // Question 1.4
 // Write a method to decide if two strings are anagrams or not.
 
-
 #include <iostream>
 #include <string>
 using namespace std;
 
-bool areAnagrams(string one, string two){
-	if(one.length() != two.length())
-		return false;
-	else{
-		int characters[128];
-		for(int i = 0; i < 128; i++)
-			characters[i] = 0;
-		bool valid = true;
-		
-		for(int i = 0; i < one.length(); i++)
-			characters[one[i]]++;
-		for(int i = 0; i < two.length(); i++)
-			characters[two[i]]--;
-		for(int i = 0; i < two.length(); i++)
-		{
-			if(characters[one[i]] > 0 )
-				valid = false;
-		}
-		return valid;
-	}
+bool areAnagrams(string one, string two)
+{
+    if (one.length() != two.length())
+        return false;
+    else
+    {
+        int characters[128];
+        for (int i = 0; i < 128; i++)
+            characters[i] = 0;
+        bool valid = true;
+
+        for (int i = 0; i < one.length(); i++)
+            characters[one[i]]++;
+        for (int i = 0; i < two.length(); i++)
+            characters[two[i]]--;
+        for (int i = 0; i < two.length(); i++)
+        {
+            if (characters[one[i]] > 0)
+                valid = false;
+        }
+        return valid;
+    }
 }
 
-int main(){
-	string one = "saecm", two = "semca";
-	cout << "IsAnagrams: " << areAnagrams(one, two);
-	
+int main()
+{
+    string one = "saecm", two = "semca";
+    cout << "IsAnagrams: " << areAnagrams(one, two);
 }
-
-
