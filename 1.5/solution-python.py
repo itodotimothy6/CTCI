7c711bb5e862407362e44987784ddfcc85b723f5
Faruq Hammed <faruqhammed8@gmail.com>
2019-09-08 17:32:16 -0500

    made the code into a function

--- 1.5/solution-python.py
+++ 1.5/solution-python.py
@@ -1,12 +1,17 @@
-#ctci 4th edition question 1.5
-#replace all the spaces in a string with "%20"
-word = input("Enter the string to convert: ")
-result = ""
-for char in word:
-	if char == " ":
-		result += "%20"
-	else:
-		result += char
-word = result
+#write a function to replace all the spaces in a sentence with "%20"
+def replace(sentence):
+	"""
+	this function URLifies a string
+	"""
+	result = ""
+	for char in sentence:
+		if char == " ":
+			result += "%20"
+		else:
+			result += char
+	return result
 
+word = "my name is faruq"
+print(word)
+word = replace(word)
 print(word)
