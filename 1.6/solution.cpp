da48cff00d4d3cbca8c01b571a8a47062d996b64
Timothy Itodo <itodotimothy6@gmail.com>
2019-09-09 07:24:20 -0500

    Solve 1.6

--- 1.6/solution.cpp
+++ 1.6/solution.cpp
@@ -7,6 +7,19 @@
 
 using namespace std;
 
+vector<vector<int> > rotateBy90(vector<vector<int> > matrix) {
+    int N = matrix.size();
+    vector<vector<int> > rotatedMatrix(N, vector<int>(N,0)); // Initialize as N*N Vector with 0s 
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            rotatedMatrix[i][j] = matrix[j][N-i-1];
+        }
+    }
+
+    return rotatedMatrix;
+}
+
 void rotateBy90_InPlace(vector<vector<int> > &matrix) {
     int N = matrix.size(), a= 0, num = N - 1;;
 
@@ -14,7 +27,7 @@ void rotateBy90_InPlace(vector<vector<int> > &matrix) {
         int b = num;
         while (b > 0){
             int i = a, j = a, x = matrix[a][a];
-
+            
             while (j < N-a-1) {
                 matrix[i][j] = matrix[i][j+1];
                 j++;
@@ -40,22 +53,11 @@ void rotateBy90_InPlace(vector<vector<int> > &matrix) {
     }
 }
 
-vector<vector<int> > rotateBy90(vector<vector<int> > matrix) {
-    int N = matrix.size();
-    vector<vector<int> > rotatedMatrix(N, vector<int>(N,0)); // Initialize as N*N Vector with 0s 
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            rotatedMatrix[i][j] = matrix[j][N-i-1];
-        }
-    }
-
-    return rotatedMatrix;
-}
-
 int main() {
 
     vector<vector<int> > matrix;
+
+    // Create a num * num matrix
     int num = 1;
     for(int i=0; i < 7; i++) {
         vector<int> rows;
@@ -65,17 +67,20 @@ int main() {
         matrix.push_back(rows);
     }
 
+    // Rotate function using new matrix
     vector<vector<int> > rMatrix = rotateBy90(matrix);
+    // Print matrix
     for (int i=0; i < matrix.size(); i++) {
         for (int j=0; j < matrix.size(); j++) {
             cout << rMatrix[i][j] << " ";
         }
         cout << endl;
     }
-
     cout << endl;
+
+    // In place rotate func
     rotateBy90_InPlace(matrix);
-    
+    // Print matrix
     for (int i=0; i < matrix.size(); i++) {
         for (int j=0; j < matrix.size(); j++) {
             cout << matrix[i][j] << " ";
