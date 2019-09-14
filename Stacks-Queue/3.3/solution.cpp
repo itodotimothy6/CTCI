e4faf14b71017c8e68298a1f951e6e5504270d5b
Timothy Itodo <itodotimothy6@gmail.com>
2019-09-14 13:15:44 -0500

    Solve 3.3

---
+++ Stacks-Queue/3.3/solution.cpp
@@ -0,0 +1,145 @@
+// Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore,
+// in real life, we would likely start a new stack when the previous stack exceeds
+// some threshold. Implement a data structure SetOfStacks that mimics this. SetOf-
+// Stacks should be composed of several stacks, and should create a new stack once
+// the previous one exceeds capacity. SetOfStacks.push() and SetOfStacks.pop() should
+// behave identically to a single stack (that is, pop() should return the same values as it
+// would if there were just a single stack).
+// FOLLOW UP
+// Implement a function popAt(int index) which performs a pop operation on a specific
+// sub-stack.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX = 4;
+
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int n) {
+        this->data = n;
+        this->next = NULL;
+    }   
+};
+
+class Stack {
+    Node* top = NULL;
+    int size = 0;
+public:
+    void push(int n) {
+        Node* newNode = new Node(n);
+        if (top) {
+            newNode->next = top;
+        }
+        
+        top = newNode;
+        size++;
+    }
+    
+    int pop() {
+        if (top) {
+            Node* n = top;
+            int data = n->data;
+            
+            top = top->next;
+            delete n;
+            size--;
+            return data;
+        }
+        
+        return -99999999;
+    }
+    
+    int getSize() {
+        return size;
+    }
+    
+    void print() {
+        Node* n = top;
+        
+        while(n) {
+            cout << n->data << " ";
+            n = n->next;
+        }
+        cout << endl;
+    }
+};
+
+
+class SetOfStacks {
+    int currentPile = -1;
+    vector<Stack> stacks;
+public:
+    // SetOfStacks() {
+    //     currentPile = 0;
+    //     Stack s = Stack();
+    //     stacks.push_back(s);
+    // }
+    
+    void push(int n) {
+        
+        if (currentPile == -1 || stacks[currentPile].getSize() == MAX) {
+            Stack s = Stack();
+            stacks.push_back(s);
+            currentPile++;
+        }
+        
+        stacks[currentPile].push(n);
+    }
+    
+    int pop() {
+        if (currentPile == -1){
+            return -99999999;
+        }
+        
+        // If pile is empty delect that stack & make
+        // prev stack the current pile
+        if (stacks[currentPile].getSize() == 0) {
+            stacks.pop_back();
+            currentPile--;
+        }
+        
+        
+        if (currentPile > -1) {
+            return stacks[currentPile].pop();
+        }
+        
+        return -99999999;
+    }
+    
+    void print() {
+        for(auto s : stacks) {
+            s.print();
+        }
+    }
+    
+    int popAt(int pileIndex) {
+        if(pileIndex < stacks.size()) return stacks[pileIndex].pop();
+        return -99999999;
+    }
+    
+};
+
+int main() {
+    
+    SetOfStacks stacks = SetOfStacks();
+    stacks.print();
+    
+    for(int i=1; i<=25; i++) {
+        stacks.push(i);
+    }
+    
+    stacks.print();
+    cout << endl;
+    
+    stacks.popAt(2);
+    
+    stacks.print();
+    cout << endl;
+    
+    
+    return 0;
+}
